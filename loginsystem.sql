77440df9097387625200b8f86478fa0fd0d06393
Manytn <96942458+Manytn@users.noreply.github.com>
2022-12-02 12:26:51 +0530

    Update loginsystem.sql

--- loginsystem.sql
+++ loginsystem.sql
@@ -1,108 +1,238 @@
 
 
+
+
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 
+--
 
-
-
-CREATE TABLE `doctorapp` (
+CREATE TABLE `clients` (
+  `clientid` varchar(60) NOT NULL,
   `fname` varchar(40) NOT NULL,
   `lname` varchar(40) NOT NULL,
+  `phoneno` varchar(10) NOT NULL,
   `email` varchar(40) NOT NULL,
-  `contact` varchar(40) NOT NULL,
-  `docapp` varchar(60) NOT NULL
+  `trainerid` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `clients`
+--
 
+INSERT INTO `clients` (`clientid`, `fname`, `lname`, `phoneno`, `email`, `trainerid`) VALUES
+('', 'Neha', 'Chaudhari', 'nc@gmail.c', '1234567890', 101);
 
-INSERT INTO `doctorapp` (`fname`, `lname`, `email`, `contact`, `docapp`) VALUES
-('Raj', 'kumar', 'kumar@gmail.com', '201', '101'),
-('saurabh', 'kumar', 'kumar121@gmail.com', '202', '102'),
-('surya', 'raj', 'raj1242gmail.com', '203', '101'),
-('Raman', 'kumar', 'raman@gmail.com', '204', '103'),
-('Aadarsh', 'thakur', 'thakur@gmail.com', '205', '103'),
-('Rahul', 'kumar', 'rahul@gmail.com', '206', '102'),
-('Sanjeev', 'Verma', 'verma12@gmail.com', '207', '103');
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `gym`
+--
 
+CREATE TABLE `gym` (
+  `gym_id` varchar(20) NOT NULL,
+  `gym_name` varchar(30) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gym`
+--
+
+INSERT INTO `gym` (`gym_id`, `gym_name`, `address`, `type`) VALUES
+('GYM1', 'GYM LAND', 'Shiv Nagar', 'men'),
+('GYM2', 'TARGET ZONE', 'Shanthi Nagar', 'unisex'),
+('GYM3', 'GEORGE GYM', 'Mahesh Nagar', 'unisex'),
+('GYM4', 'SUNNY GYM FITNESS STATION', 'Rupali Complex', 'women'),
+('GYM5', 'A3 FITNESS GYM', 'Ramnagar Colony', 'men'),
+('GYM6', 'SHAPE GYM', 'Zion Colony', 'unisex'),
+('GYM7', 'TITAN GYM', 'Old City', 'women'),
+('GYM8', 'TIGERS TOP GYM', 'Madival Circle', 'men');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
+  `uname` varchar(30) NOT NULL,
+  `pwd` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `uname`, `pwd`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintb`
+--
 
 CREATE TABLE `logintb` (
   `username` varchar(40) NOT NULL,
   `password` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Dumping data for table `logintb`
+--
 
 INSERT INTO `logintb` (`username`, `password`) VALUES
 ('admin', 'pass');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `member`
+--
 
+CREATE TABLE `member` (
+  `mem_id` varchar(20) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `dob` varchar(20) DEFAULT NULL,
+  `age` varchar(20) DEFAULT NULL,
+  `package` varchar(10) DEFAULT NULL,
+  `mobileno` varchar(10) DEFAULT NULL,
+  `pay_id` varchar(20) DEFAULT NULL,
+  `trainer_id` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Package` (
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`mem_id`, `name`, `dob`, `age`, `package`, `mobileno`, `pay_id`, `trainer_id`) VALUES
+('M1', 'Aditya', '18/08/1994', '26', '5200', '8888888888', 'Payment1', 'T1'),
+('M2', 'Karan', '26/06/1998', '21', '4800', '9988998899', 'Payment2', 'T2'),
+('M3', 'Chirag', '22/07/1997', '22', '6400', '9977997799', 'Payment3', 'T3'),
+('M4', 'Abhishek', '21/08/1998', '21', '5400', '9966996699', 'Payment4', 'T4'),
+('M5', 'Veeresh', '24/06/1999', '20', '6000', '9955995599', 'Payment5', 'T5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `package`
+--
+
+CREATE TABLE `package` (
   `Package_id` varchar(40) NOT NULL,
   `Package_name` varchar(40) NOT NULL,
   `Amount` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `package`
+--
 
-
-INSERT INTO `Package` (`Package_id`, `Package_name`, `Amount`) VALUES
+INSERT INTO `package` (`Package_id`, `Package_name`, `Amount`) VALUES
 ('121', 'preliminary', 800),
 ('122', 'Wt. gain', 1500),
 ('123', 'Wt.loss', 1000);
 
+-- --------------------------------------------------------
 
-CREATE TABLE `Payment` (
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
   `Payment_id` int(10) NOT NULL,
   `Amount` int(20) NOT NULL,
   `customer_id` varchar(20) NOT NULL,
   `payment_type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `payment`
+--
 
-INSERT INTO `Payment` (`Payment_id`, `Amount`, `customer_id`, `payment_type`) VALUES
+INSERT INTO `payment` (`Payment_id`, `Amount`, `customer_id`, `payment_type`) VALUES
 (301, 1500, '201', 'cash'),
 (302, 800, '202', 'card'),
 (303, 1000, '203', 'cheque'),
-(304, 1500, '204', 'cash');
+(304, 1500, '204', 'cash'),
+(308, 10000, '201', 'cash'),
+(805, 1000, '190', 'cash'),
+(808, 3000, '123', 'cash');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `trainer`
+--
 
-CREATE TABLE `Trainer` (
+CREATE TABLE `trainer` (
   `Trainer_id` int(20) NOT NULL,
   `Name` varchar(40) NOT NULL,
-  `phone` int(100) NOT NULL
+  `phone` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `trainer`
+--
 
+INSERT INTO `trainer` (`Trainer_id`, `Name`, `phone`) VALUES
+(101, 'Rakesh', '12365489'),
+(102, 'Ravi', '21365789'),
+(103, 'wasim', '123564789'),
+(104, 'Sameer', '12536987'),
+(123, 'rakesh', '2147483647'),
+(125, 'ram', '2147483647'),
+(783, 'neha', '4567898765'),
+(907, 'rani', '4567898765');
 
-INSERT INTO `Trainer` (`Trainer_id`, `Name`, `phone`) VALUES
-(101, 'Rakesh', 12365489),
-(102, 'Ravi', 21365789),
-(103, 'wasim', 123564789),
-(104, 'Sameer', 12536987);
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`clientid`),
+  ADD KEY `trainerid` (`trainerid`);
 
-ALTER TABLE `doctorapp`
-  ADD PRIMARY KEY (`contact`);
-
-
-ALTER TABLE `Package`
+--
+-- Indexes for table `package`
+--
+ALTER TABLE `package`
   ADD PRIMARY KEY (`Package_id`);
 
-
-ALTER TABLE `Payment`
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
   ADD PRIMARY KEY (`Payment_id`);
 
+--
+-- Indexes for table `trainer`
+--
+ALTER TABLE `trainer`
+  ADD FOREIGN KEY (`Trainer_id`);
 
-ALTER TABLE `Trainer`
-  ADD PRIMARY KEY (`Trainer_id`);
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`trainerid`) REFERENCES `trainer` (`Trainer_id`);
 COMMIT;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
